56761c9844d4a5687dcbef50d1d89b01a54eeda3
fatihturker <fatih.turker@optiva.com>
2021-01-01 21:52:23 +0300

    Add analytics log

---
+++ src/app/service/analytics/analytics.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { LoadingService } from "../loading/loading.service";
+import { environment } from "../../../environments/environment";
+
+@Injectable({
+  providedIn: "root",
+})
+export class AnalyticsService {
+  constructor(
+    private http: HttpClient,
+    private loadingService: LoadingService
+  ) {}
+
+  logRegisteredUser(userData: any) {
+    return this.http.post<any>(`${environment.serverUrl}/event`, {
+      name: "USER_REGISTERED",
+      payload: userData,
+    });
+  }
+}
