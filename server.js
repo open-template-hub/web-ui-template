003098f24c4dc9343837011c18ab745747fad96e
Furkan Yavuz <furknyavuz@gmail.com>
2021-02-12 17:42:25 +0300

    Name changes

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/basic-angular-ui-template'));
+app.use(express.static(__dirname + '/dist/web-ui-ui-template'));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/basic-angular-ui-template/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/web-ui-ui-template/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
