c2086d700f946ca9814cc20d415b0c05fbc4703d
Furkan Yavuz <furknyavuz@gmail.com>
2022-03-02 22:00:53 +0300

    fix indentation

--- src/app/page/landing-layout/home-page/home-page.component.ts
+++ src/app/page/landing-layout/home-page/home-page.component.ts
@@ -100,7 +100,6 @@ export class HomePageComponent {
     .then( counters => {
       this.isCountersLoading = false;
       this.counters = counters;
-      console.log(this.counters);
     } )
     .catch( error => {
       this.isCountersLoading = false;
